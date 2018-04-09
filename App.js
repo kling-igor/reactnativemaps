92111df2fbe52c66550a8ac526408953760f17f3
Igor Kling <klingiv@altarix.ru>
2018-04-09 18:13:11 +0400

    working implementation

--- App.js
+++ App.js
@@ -1,208 +1,240 @@
-import React, { Component } from 'react';
-import {View, Text, StyleSheet, Dimensions} from 'react-native';
-import MapView, { Callout, Marker, ProviderPropType, PROVIDER_GOOGLE } from 'react-native-maps';
-import Geocoder from './Geocoder'
-import {v4} from 'uuid'
+import React, { Component } from 'react'
+import { View, StyleSheet, Dimensions } from 'react-native'
+import MapView, { Callout, Marker, ProviderPropType, PROVIDER_GOOGLE } from 'react-native-maps'
+import PropTypes from 'prop-types'
+import { observable, computed, action, autorun, transaction, when, spy } from 'mobx'
+import { observer, inject, PropTypes as PropTypesMobX, Provider, Observer } from 'mobx-react'
+import v4 from 'uuid'
+
+spy((event) => {
+  if (event.type === 'action') {
+    console.log(`[SPY]: ${event.name} with args: `, event.arguments)
+  }
+})
 
 
-let { width, height } = Dimensions.get('window')
-const ASPECT_RATIO = width / height
-const LATITUDE_DELTA = 0.006
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+class MapStore {
+  width
+  height
+  @observable viewRatio
+  @observable.ref center = { lat: 55.75149154293644, lng: 37.611341682501916 }
+  @observable zoom = 15
 
-const API_KEY = 'AIzaSyAEoP8nFpwQaxIJzalsEU6pXYB5JnR_T5c';
+  @observable.ref markers = []
 
-Geocoder.setApiKey(API_KEY);
+  // ready = computed(() => {
+  //   console.log('!!!viewRatio:', this)
+  //   return false
+  //   // return this.viewRatio
+  // }, { context: this })
 
-// let zoom = Math.round(Math.log(360 / region.longitudeDelta) / Math.LN2)
+  // get ready() {
+  //     console.log('!!!READY:', this.viewRatio)
+  //     return this.viewRatio !== undefined
+  //   }
 
-const zoom = longitudeDelta => Math.round(Math.log(360 / longitudeDelta) / Math.LN2)
+  @computed get region() {
+    console.log('---REGION---')
 
+    const ratio = this.viewRatio
 
-export default class App extends React.Component {
+    console.log('RATIO:', ratio)
+    console.log('WIDTH:', this.width)
+    console.log('ZOOM:', this.zoom);
 
-  constructor(props) {
-    super(props)
+    if (ratio) {
+      // console.log('READY!!!')
+      const latitudeDelta = this.width * 360 / (Math.exp(this.zoom * Math.LN2) * 256)
+      const longitudeDelta = latitudeDelta * ratio
 
-    this.state = {
-      region:{
-        latitude: 53.54149059188792,
-        longitude: 49.36498571187258,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA
-      },
-      poi: null,
-      address: null,
-      markers:[
-        {
-          latitude: 53.541,
-          longitude: 49.365,
-          address: 'NO ADDRESS'
-        },
-        // {
-        //   latitude: 53.54149059188792,
-        //   longitude: 49.36498571187258,
-        //   address: ''
-        // }
-      ]
+      return { latitude: this.center.lat, longitude: this.center.lng, latitudeDelta, longitudeDelta }
     }
 
-    this.onRegionChange = this.onRegionChange.bind(this)
-    // this.onPoiClick = this.onPoiClick.bind(this)
+    console.log('NOT READY')
 
-    const {region:{latitude, longitude}} = this.state
-
-    console.log(`lat: ${latitude} lng: ${longitude}`)
-
-
-    // TODO если к окончанию обновления изменится массив маркеров - то проблема!!!!
-
-    const updateMarkerAddress = (index, markers) => {
-
-      if (!markers[index]) return
-
-      const {latitude, longitude} = markers[index]
-
-      console.log(`REQUESTING ADDRES FOR lat: ${latitude},  lang:${longitude}`)
-
-
-      Geocoder.getFromLatLng(latitude, longitude).then(
-        json => {
-          // console.log('JSON:', json)
-          const address = json.results[0].formatted_address;
-          console.log('ADDRESS:', address)
-
-          const updatedMarkers = [...markers.slice(0, index), 
-            {
-              latitude,
-              longitude,
-              address
-            },
-            ...markers.slice(index + 1)]
-
-          this.setState({
-            markers: updatedMarkers
-          }, () =>{
-
-            console.log('UPDATED MARKERS:', this.state.markers)
-
-            if (index < this.state.markers.length - 1) {
-              setTimeout(updateMarkerAddress, 0, index + 1, [...this.state.markers])
-            }
-          })
-        },
-        error => {
-          console.error(error)
-        })
-    }
-
-    // updateMarkerAddress(0, [...this.state.markers])
-
+    return { latitude: 55.75149154293644, longitude: 37.611341682501916, latitudeDelta: 0.006, longitudeDelta: 0.012 }
   }
 
-  // onPoiClick(e) {
-  //   console.log('On poi click')
+  @action
+  onLayout(event) {
+    const { width, height } = event.nativeEvent.layout
+    this.width = width
+    this.height = height
 
-  //   const poi = e.nativeEvent;
+    this.viewRatio = width / height
+    console.log('on layout:', this.width, this.height);
+    console.log('RATIO:', this.viewRatio)
+  }
 
-  //   this.setState({
-  //     poi
-  //   })
-  // }
+  @action
+  setCenter(lat, lng) {
+    this.center = { lat, lng }
+  }
 
-	onRegionChange(region) {
-    console.log(`region: ${JSON.stringify(region)}, zoom: ${zoom(region.longitudeDelta)}`)
-    // this.setState({
-    //   region:{
-    //     latitude: region.latitude || this.state.latitude,
-    //     longitude: region.longitude || this.state.longitude,
-    //     latitudeDelta: LATITUDE_DELTA,
-    //     longitudeDelta: LONGITUDE_DELTA
-    //   }
-    // })
-	}
+  @action
+  setZoom(zoom) {
+    this.zoom = zoom
+  }
 
-  componentDidMount() {
-		this.watchID = navigator.geolocation.watchPosition((position) => {
-		// Create the object to update this.state.mapRegion through the onRegionChange function
-      this.setState({
-        region:{
-          latitude: position.coords.latitude || this.state.latitude,
-          longitude: position.coords.longitude || this.state.longitude,
-          latitudeDelta: LATITUDE_DELTA,
-          longitudeDelta: LONGITUDE_DELTA
-        }
-      })
-		})
-	}
+  @action
+  setCenterAndZoom(lat, lng, zoom) {
+    transaction(() => {
+      this.setCenter(lat, lng)
+      this.setZoom(zoom)
+      console.log('setCenterAndZoom ', lat, lng, zoom)
+    })
+  }
 
-  componentWillUnmount() {
-		if (this.watchID) navigator.geolocation.clearWatch(this.watchID);
-	}
+  @action
+  addMarker(latitude, longitude, title = 'MARKER') {
+    const marker = {
+      latitude,
+      longitude,
+      id: v4()
+    }
+    this.markers = [...this.markers, marker]
+  }
 
-  // standard, satellite, hybrid, terrain
-
-
-  // title={`${this.state.region.longitude.toFixed(3) } / ${ this.state.region.latitude.toFixed(3)}`}
-  // onDragEnd={e => console.log(`placed to: ${e.nativeEvent.coordinate}`)}
-  render() {
-    return (
-      <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_GOOGLE}
-          style={styles.map}
-          mapType={'standard'}
-          showsUserLocation={true}
-          followUserLocation={true}
-          region={this.state.region}
-          onRegionChangeComplete={this.onRegionChange}
-        >
-          {this.state.poi && (
-            <Marker
-              coordinate={this.state.poi.coordinate}
-            >
-              <Callout>
-                <View>
-                  <Text>Place Id: { this.state.poi.placeId }</Text>
-                  <Text>Name: { this.state.poi.name }</Text>
-                </View>
-              </Callout>
-            </Marker>  
-          )}
-
-          {this.state.markers.map(marker => (
-            <Marker
-              dragable
-              title={`SOME TITLE`}
-              coordinate={{
-                latitude: marker.latitude,
-                longitude: marker.longitude
-              }}
-              onSelect={({nativeEvent}) => console.log('onSelect', nativeEvent.coordinate)}
-              onDrag={({nativeEvent}) => console.log('onDrag', nativeEvent.coordinate)}
-              onDragStart={({nativeEvent}) => console.log('onDragStart', nativeEvent.coordinate)}
-              onDragEnd={({nativeEvent}) => console.log('onDragEnd', nativeEvent.coordinate)}
-              onPress={({nativeEvent}) => console.log('onPress', nativeEvent.coordinate)}
-              key={v4()}
-            >
-          </Marker>
-          ))}
-        </MapView>
-      </View>
-    )
+  onMarkerPress(markerId) {
+    const marker = store.markers.find(marker => marker.id === markerId)
+    if (marker) {
+      const { latitude, longitude } = marker
+      setCenter(latitude, longitude)
+    }
   }
 }
 
 
 
+const Markers = props => {
+  if (!props.markers.length) return null
 
-const styles = StyleSheet.create ({
+  const bind = (id, onClick) => () => {
+    onClick(id)
+  }
+
+  return props.markers.map(marker => {
+    const { latitude, longitude, id } = marker
+    return <Marker coordinate={{ latitude, longitude }} key={id} onMarkerPress={bind(marker.id, props.onMarkerPress)} />
+  })
+}
+
+// Markers.propTypes = {
+//   markers: PropTypes.observableArray.isRequired
+// }
+
+
+const MapRenderer = observer(({ style, mapStore, onRegionChangeComplete, onPress }) => (
+  <MapView
+    style={styles.map}
+    provider={PROVIDER_GOOGLE}
+    mapType='standard'
+    startRegion={mapStore.region}
+    onRegionChangeComplete={onRegionChangeComplete}
+    onPress={onPress}
+  >
+    <Markers markers={mapStore.markers} onMarkerPress={mapStore.onMarkerPress} />
+  </MapView>
+))
+
+@inject("mapStore")
+@observer
+class MapScreen extends React.Component {
+
+  static propTypes = {
+    onRegionChange: PropTypes.func,
+    markers: PropTypes.array
+  }
+
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      region: { latitude: 0, longitude: 0, latitudeDelta: 0.006, longitudeDelta: 0.012 }
+    }
+  }
+
+  componentWillReact() {
+    console.log("MapScreen re-render, since the store has changed!");
+  }
+
+  onRegionChangeComplete = region => {
+    console.log('onRegionChangeComplete:', region)
+    // this.setState({
+    //   region
+    // }, () => {
+    //   console.log('STATE:', this.state.region)
+    //   const { latitude, longitude, longitudeDelta } = region
+    //   const zoom = Math.log(360 / longitudeDelta) / Math.LN2
+    //   // store.setCenter(latitude, longitude)
+    //   // store.setZoom(zoom)
+    //   this.props.mapStore.setCenterAndZoom(latitude, longitude, zoom)
+    // })
+
+    const { latitude, longitude, longitudeDelta } = region
+    const zoom = Math.log(360 / longitudeDelta) / Math.LN2
+    this.props.mapStore.setCenterAndZoom(latitude, longitude, zoom)
+  }
+
+  onPress = e => {
+    const { latitude, longitude } = e.nativeEvent.coordinate
+    console.log(`CLICK ON MAP AT: ${latitude} ${longitude}`)
+    this.props.mapStore.addMarker(latitude, longitude)
+  }
+
+  onMarkerPress = markerId => {
+    console.log('on marker press:', markerId)
+  }
+
+  //startRegion={this.props.store.region}
+
+  // startRegion={mapStore.region}
+
+  // render() {
+  //   const { mapStore, style } = this.props
+  //   const ready = this.props.mapStore.viewRatio
+  //   console.log('ready:', ready)
+  //   return (
+  //     <View style={[style, styles.container]} onLayout={mapStore.onLayout}>
+  //       {true ? <MapView
+  //         style={styles.map}
+  //         provider={PROVIDER_GOOGLE}
+  //         mapType='standard'
+  //         region={mapStore.region}
+  //         onRegionChangeComplete={this.onRegionChangeComplete}
+  //         onPress={this.onPress}
+  //       >
+  //         <Markers markers={mapStore.markers} onMarkerPress={mapStore.onMarkerPress} />
+  //       </MapView> : null}
+  //     </View >)
+  // }
+
+  render() {
+    const { mapStore, style } = this.props
+    return (
+      <View style={[style, styles.container]} onLayout={mapStore.onLayout.bind(mapStore)}>
+        {mapStore.viewRatio ? <MapRenderer style={style} mapStore={this.props.mapStore} onRegionChangeComplete={this.onRegionChangeComplete} onPress={this.onPress} /> : null}
+      </View >)
+  }
+}
+
+
+const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
-	map: {
-		...StyleSheet.absoluteFillObject,
-	}
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  }
 })
+
+const store = new MapStore
+
+export default () => (
+  <Provider mapStore={store}>
+    <MapScreen />
+  </Provider>
+)
